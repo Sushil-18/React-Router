58b5fd564cff295f6c76d66dec761cce87f456e2
sushil-18 <sushilkumarwaghmare9@gmail.com>
2024-05-31 23:50:16 +0530

    Building HomePage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import HomePageImg from "../../assets/Home_Image.png";
 
 const Home = () => {
   return (
-    <div className="flex justify-between">
-      <aside>
+    <div className="flex justify-between mt-8">
+      <aside className="relative">
         <div>
           <div>
-            <h2>Download Now</h2>
+            <h2 className="relative left-0 bottom-0">Download Now</h2>
             <span>Lorel Ipsum</span>
           </div>
           <button>Download now</button>
         </div>
+        <img src={HomePageImg} alt="HomePage Image"></img>
       </aside>
     </div>
   );
